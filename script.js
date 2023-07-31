74832938ba72be39ec899204cabb2117ceb0ffee
Joshua Owusu <joshbillion1@gmail.com>
2023-07-31 10:58:41 +0000

    Commit auto_update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var tl = gsap.timeline({
   scrollTrigger: {
     trigger: "#main",
     start: "50% 50%",
-    end: "100% 50%",
+    end: "150% 50%",
     scrub: 2,
     pin: true,
     markers: true,
