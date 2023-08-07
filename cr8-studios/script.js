e5d7fdfec5cc21b8c5d4b17dc17aae276f226d64
Joshua Owusu <joshbillion1@gmail.com>
2023-08-07 08:33:57 +0000

    Commit auto_update

--- cr8-studios/script.js
+++ cr8-studios/script.js
@@ -175,50 +175,22 @@ document.querySelector("#btnsm").addEventListener("mouseleave", function () {
 });
 
 // section
-// const span = document.querySelector(".hover-btn2");
-// const overlay = document.querySelector(".overlay");
+const span = document.querySelector(".hover-btn2");
+const overlay = document.querySelector(".overlay");
 
-// window.addEventListener("mousemove", (e) => {
-//   const { clientX, clientY } = e;
-//   const x = Math.round((clientX / window.innerWidth) * 100);
-//   const y = Math.round((clientY / window.innerHeight) * 100);
+window.addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+  const x = Math.round((clientX / window.innerWidth) * 100);
+  const y = Math.round((clientY / window.innerHeight) * 100);
 
-//   gsap.to(overlay, {
-//     "--x": `${x}%`,
-//     "--y": `${y}%`,
-//     duration: 0.3,
-//     ease: "sine.out",
-//   });
-// });
-
-// span.addEventListener("click", () => {
-//   overlay.classList.toggle("is-open");
-// });
-
-function setupHoverEffect() {
-  const span = document.querySelector(".hover-btn2");
-  const overlay = document.querySelector(".overlay");
-
-  window.addEventListener("mousemove", (e) => {
-    const { clientX, clientY } = e;
-    const x = Math.round((clientX / window.innerWidth) * 100);
-    const y = Math.round((clientY / window.innerHeight) * 100);
-
-    gsap.to(overlay, {
-      "--x": `${x}%`,
-      "--y": `${y}%`,
-      duration: 0.3,
-      ease: "sine.out",
-    });
+  gsap.to(overlay, {
+    "--x": `${x}%`,
+    "--y": `${y}%`,
+    duration: 0.3,
+    ease: "sine.out",
   });
+});
 
-  span.addEventListener("click", () => {
-    overlay.classList.toggle("is-open");
-  });
-}
-
-// Check if the element with class "hover-btn2" is present on the page
-if (document.querySelector(".hover-btn2")) {
-  // Call the setupHoverEffect function only on pages that contain the element with class "hover-btn2"
-  setupHoverEffect();
-}
+span.addEventListener("click", () => {
+  overlay.classList.toggle("is-open");
+});
