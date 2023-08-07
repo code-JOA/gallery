2950b5f41bb4d7b49e02bf40275b7c6c0e3b443c
Joshua Owusu <joshbillion1@gmail.com>
2023-08-07 08:20:53 +0000

    Commit auto_update

--- cr8-studios/script.js
+++ cr8-studios/script.js
@@ -173,3 +173,24 @@ document.querySelector("#btnsm").addEventListener("mouseleave", function () {
   document.querySelector("#btnsm").style.backgroundColor = "initial";
   document.querySelector("#btnsm h1").style.color = "rgb(95, 95, 95)";
 });
+
+// section
+const span = document.querySelector(".hover-btn2");
+const overlay = document.querySelector(".overlay");
+
+window.addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+  const x = Math.round((clientX / window.innerWidth) * 100);
+  const y = Math.round((clientY / window.innerHeight) * 100);
+
+  gsap.to(overlay, {
+    "--x": `${x}%`,
+    "--y": `${y}%`,
+    duration: 0.3,
+    ease: "sine.out",
+  });
+});
+
+span.addEventListener("click", () => {
+  overlay.classList.toggle("is-open");
+});
