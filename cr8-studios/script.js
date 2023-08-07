65a056ef82d6059238ee35fd96afc05cf26c3766
Joshua Owusu <joshbillion1@gmail.com>
2023-08-07 09:11:47 +0000

    Commit auto_update

--- cr8-studios/script.js
+++ cr8-studios/script.js
@@ -53,27 +53,6 @@ tl.to(
   "a"
 );
 
-// hover mask section
-const span = document.querySelector(".hover-btn2");
-const overlay = document.querySelector(".overlay");
-
-window.addEventListener("mousemove", (e) => {
-  const { clientX, clientY } = e;
-  const x = Math.round((clientX / window.innerWidth) * 100);
-  const y = Math.round((clientY / window.innerHeight) * 100);
-
-  gsap.to(overlay, {
-    "--x": `${x}%`,
-    "--y": `${y}%`,
-    duration: 0.3,
-    ease: "sine.out",
-  });
-});
-
-span.addEventListener("click", () => {
-  overlay.classList.toggle("is-open");
-});
-
 // black circle
 document
   .querySelector("#aboutme")
