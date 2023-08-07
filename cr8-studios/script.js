cf652999f584a9d44813c00859aa1f019e7d925a
Joshua Owusu <joshbillion1@gmail.com>
2023-08-07 09:03:20 +0000

    Commit auto_update

--- cr8-studios/script.js
+++ cr8-studios/script.js
@@ -1,3 +1,23 @@
+const span = document.querySelector(".hover-btn2");
+const overlay = document.querySelector(".overlay");
+
+window.addEventListener("mousemove", (e) => {
+  const { clientX, clientY } = e;
+  const x = Math.round((clientX / window.innerWidth) * 100);
+  const y = Math.round((clientY / window.innerHeight) * 100);
+
+  gsap.to(overlay, {
+    "--x": `${x}%`,
+    "--y": `${y}%`,
+    duration: 0.3,
+    ease: "sine.out",
+  });
+});
+
+span.addEventListener("click", () => {
+  overlay.classList.toggle("is-open");
+});
+
 var tl = gsap.timeline({
   scrollTrigger: {
     trigger: "#main",
@@ -175,22 +195,22 @@ document.querySelector("#btnsm").addEventListener("mouseleave", function () {
 });
 
 // section
-const span = document.querySelector(".hover-btn2");
-const overlay = document.querySelector(".overlay");
+// const span = document.querySelector(".hover-btn2");
+// const overlay = document.querySelector(".overlay");
 
-window.addEventListener("mousemove", (e) => {
-  const { clientX, clientY } = e;
-  const x = Math.round((clientX / window.innerWidth) * 100);
-  const y = Math.round((clientY / window.innerHeight) * 100);
+// window.addEventListener("mousemove", (e) => {
+//   const { clientX, clientY } = e;
+//   const x = Math.round((clientX / window.innerWidth) * 100);
+//   const y = Math.round((clientY / window.innerHeight) * 100);
 
-  gsap.to(overlay, {
-    "--x": `${x}%`,
-    "--y": `${y}%`,
-    duration: 0.3,
-    ease: "sine.out",
-  });
-});
+//   gsap.to(overlay, {
+//     "--x": `${x}%`,
+//     "--y": `${y}%`,
+//     duration: 0.3,
+//     ease: "sine.out",
+//   });
+// });
 
-span.addEventListener("click", () => {
-  overlay.classList.toggle("is-open");
-});
+// span.addEventListener("click", () => {
+//   overlay.classList.toggle("is-open");
+// });
