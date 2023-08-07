a01e70dd32324d3da54f50fa04d901fcbf019f4e
Joshua Owusu <joshbillion1@gmail.com>
2023-08-07 08:30:39 +0000

    Commit auto_update

--- cr8-studios/script.js
+++ cr8-studios/script.js
@@ -199,20 +199,25 @@ function setupHoverEffect() {
   const span = document.querySelector(".hover-btn2");
   const overlay = document.querySelector(".overlay");
 
-  window.addEventListener("mousemove", (e) => {
-    const { clientX, clientY } = e;
-    const x = Math.round((clientX / window.innerWidth) * 100);
-    const y = Math.round((clientY / window.innerHeight) * 100);
+  if (span && overlay) {
+    window.addEventListener("mousemove", (e) => {
+      const { clientX, clientY } = e;
+      const x = Math.round((clientX / window.innerWidth) * 100);
+      const y = Math.round((clientY / window.innerHeight) * 100);
 
-    gsap.to(overlay, {
-      "--x": `${x}%`,
-      "--y": `${y}%`,
-      duration: 0.3,
-      ease: "sine.out",
+      gsap.to(overlay, {
+        "--x": `${x}%`,
+        "--y": `${y}%`,
+        duration: 0.3,
+        ease: "sine.out",
+      });
     });
-  });
 
-  span.addEventListener("click", () => {
-    overlay.classList.toggle("is-open");
-  });
+    span.addEventListener("click", () => {
+      overlay.classList.toggle("is-open");
+    });
+  }
 }
+
+// Call the setupHoverEffect function to enable the mouse interaction all over the website
+setupHoverEffect();
